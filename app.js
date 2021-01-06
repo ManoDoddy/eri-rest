4f486b51de4bde377ca3acd8c3325354fb7dc905
Vinicius Gadelha dos Santos <vinibr242@hotmail.com>
2021-01-06 17:02:05 -0300

    CORS update

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 const charactersRoute = require('./routes/character')
 
 app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+
+    if(req.method == 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).send({})
+    }
+
+    next()
+})
+
 app.use('/character', charactersRoute)
 
 app.use((req, res, next) => {
